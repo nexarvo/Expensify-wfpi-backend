13c6aa89379a3b79df7a49f42fa50f2ef20b9ecf
usman-ghani564 <usmanghani564.ug9@gmail.com>
2024-05-09 03:12:03 +0500

    setup logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WaitingForProposalIssuesModule } from './waiting-for-proposals-issues/waiting-for-proposals-issues.module';
+import { LoggerModule } from './utils/logger/logger.module';
 
 @Module({
-  imports: [WaitingForProposalIssuesModule],
+  imports: [
+    WaitingForProposalIssuesModule,
+    LoggerModule,
+  ],
 })
 export class AppModule {}
