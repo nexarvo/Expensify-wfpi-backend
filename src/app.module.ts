f6b747973c050ea96c4bd2ae0a575d21f4707121
usman-ghani564 <usmanghani564.ug9@gmail.com>
2024-05-09 03:27:30 +0500

    setup axios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WaitingForProposalIssuesModule } from './waiting-for-proposals-issues/waiting-for-proposals-issues.module';
 import { LoggerModule } from './utils/logger/logger.module';
+import { AxiosModule } from './utils/axios/axios.module';
 
 @Module({
   imports: [
     WaitingForProposalIssuesModule,
     LoggerModule,
+    AxiosModule,
   ],
 })
 export class AppModule {}
